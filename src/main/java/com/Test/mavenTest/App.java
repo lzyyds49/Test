bb6f21a36453c49da72095042279c0ebd9f77519
lzyyds49 <19573516181@163.com>
2024-07-04 19:02:58 +0800

    change1111

--- src/main/java/com/Test/mavenTest/App.java
+++ src/main/java/com/Test/mavenTest/App.java
@@ -15,5 +15,9 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println( "change" );
+        
+        System.out.println( "change" );
+        System.out.println( "change" );
+        System.out.println( "change" );
     }
 }
